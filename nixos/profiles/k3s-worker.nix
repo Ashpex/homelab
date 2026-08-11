a7e5d5f5caf824baa27746c575f73d53ecbc7b8b
Ashpex <ashpex@pm.me>
2026-08-12 00:39:28 +0700

    fix

--- nixos/profiles/k3s-worker.nix
+++ nixos/profiles/k3s-worker.nix
@@ -7,7 +7,6 @@
     serverAddr = "https://192.168.1.110:6443";
     tokenFile = "/etc/rancher/k3s/node-token";
     nodeLabels = [
-      "node-role.kubernetes.io/worker=true"
       "homelab.node/role=worker"
       "homelab.storage/ssd=true"
       "homelab.storage/nfs-client=true"
